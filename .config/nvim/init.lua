3e23fbd4c3b7bdb4663cda5882e3f3ba16e45aac
Jianshu Zhao <101102995+ZhaoJianshu@users.noreply.github.com>
2024-03-22 16:40:39 +0800

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,7 @@ vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 -- UI config
 vim.opt.number = true               -- show absolute number
 vim.opt.relativenumber = true       -- add numbers to each line on the left side
+
 vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
 vim.opt.splitbelow = true           -- open new vertical split bottom
 vim.opt.splitright = true           -- open new horizontal splits right
